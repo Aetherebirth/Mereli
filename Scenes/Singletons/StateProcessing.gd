ed215d58a9d666d4aa9c08af77c866258779f611
Ninjdai <ninjdai@duck.com>
2024-06-08 08:46:58 +0200

    refactor(entities): Change world state structure to account for entities

--- Scenes/Singletons/StateProcessing.gd
+++ Scenes/Singletons/StateProcessing.gd
@@ -4,11 +4,11 @@ class_name StateProcessing
 var world_state = {}
 
 func _physics_process(delta):
-	if not get_parent().player_state_collection.is_empty():
-		world_state = get_parent().player_state_collection.duplicate(true)
-		for player_id in world_state.keys():
-			world_state[player_id].erase("T")
-			world_state[player_id]["D"] = {}
+	if not get_parent().entity_state_collection.players.is_empty():
+		world_state = get_parent().entity_state_collection.duplicate(true)
+		for player_id in world_state.players.keys():
+			world_state.players[player_id].erase("T")
+			world_state.players[player_id]["D"] = {}
 		world_state["T"] = int(Time.get_unix_time_from_system()*1000)
 		# Verifications
 		# Anti-Cheat
